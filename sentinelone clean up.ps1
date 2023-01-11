57bae523106e0a3e548e61686229569993cf8c2b
Flash Fanatic <121263173+FlashFanatic@users.noreply.github.com>
2023-01-11 14:22:42 -0800

    Add files via upload

---
+++ sentinelone clean up.ps1
@@ -0,0 +1,11 @@
+﻿Remove-Item "C:\WindowsService1.exe" -Force
+
+Remove-Item "C:\WindowsService1.InstallLog" -Force
+
+Remove-Item "C:\WindowsService1.InstallState" -Force
+
+Remove-Item "C:\SentinelUninstaller.exe" -Force
+
+Remove-Item 'HKLM:\\SYSTEM\CurrentControlSet\Control\SafeBoot\Minimal\Service1' -Force
+
+Start-Process 'cmd.exe' -ArgumentList '/C sc delete service1' -Wait
